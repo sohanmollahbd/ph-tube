dabfeea4a92bd56a59269536b57d8d3be9b6e8ae
sohanmollahbd <sohanmollahbd@gmail.com>
2024-10-10 23:37:59 +0600

    Added dynamic cards

--- scripts/video.js
+++ scripts/video.js
@@ -20,10 +20,54 @@ const loadVideos = () => {
     .catch((error) => console.log(error));
 };
 
+// const cardDemo = {
+//   "category_id": "1001",
+//   "video_id": "aaaa",
+//   "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//   "title": "Shape of You",
+//   "authors": [
+//       {
+//           "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//           "profile_name": "Olivia Mitchell",
+//           "verified": ""
+//       }
+//   ],
+//   "others": {
+//       "views": "100K",
+//       "posted_date": "16278"
+//   },
+//   "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
+
 const displayVideos = (videos) => {
-   const videosContainer = document.getElementById("videos");
+  const videosContainer = document.getElementById("videos");
   videos.forEach((video) => {
-console.log(video);
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+        <figure class="h-[200px] relative">
+        <img class="h-full w-full object-cover"
+          src=${video.thumbnail}
+          alt="Shoes" />
+          <span class="absolute right-2 bottom-2 bg-black p-1 rounded text-white">${video.others.posted_date}</span>
+      </figure>
+      <div class="px-0 py-2 flex gap-2">
+        <div>
+        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
+        </div>
+        <div>
+        <h2 class="font-bold">${video.title}</h2>
+        <div class="flex items-center gap-2">
+          <P class="text-gray-400">${video.authors[0].profile_name}</P>
+          ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />` : ""}
+        </div>
+        <P></P>
+        </div>
+        
+      </div>
+    `;
+    videosContainer.append(card);
   });
 };
 
---
+++ test.js
@@ -0,0 +1,11 @@
+const isVerified = "";
+
+// if(isVerified === true){
+//    console.log("User is verified");
+// }
+// else{
+//    console.log("User is not verified");
+
+// }
+
+console.log(`${isVerified === true? "User is verified" : "User is not verified"}`);
