b3f3f897c77d646cf3b8dbb1a74d4549b22cfaee
sohanmollahbd <sohanmollahbd@gmail.com>
2024-10-12 11:50:09 +0600

    Load and display videos based on category

--- scripts/video.js
+++ scripts/video.js
@@ -1,5 +1,15 @@
 console.log("Video script added");
 
+//  second to hours and minutes
+function getTimeString(time) {
+  // get hour and rest seconds
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
 // 1. Fetch, load and show categories on html
 
 // create loadCategories /GET: Categories
@@ -17,6 +27,17 @@ const loadVideos = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
     .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+// load category videos
+const loadCategoryVideos = (id) => {
+  // alert(id);
+
+  // fetch the data
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category))
     .catch((error) => console.log(error));
 };
 
@@ -41,6 +62,7 @@ const loadVideos = () => {
 
 const displayVideos = (videos) => {
   const videosContainer = document.getElementById("videos");
+  videosContainer.innerHTML ="";
   videos.forEach((video) => {
     console.log(video);
     const card = document.createElement("div");
@@ -50,17 +72,30 @@ const displayVideos = (videos) => {
         <img class="h-full w-full object-cover"
           src=${video.thumbnail}
           alt="Shoes" />
-          <span class="absolute right-2 bottom-2 bg-black p-1 rounded text-white">${video.others.posted_date}</span>
+          ${
+            video.others.posted_date?.length == 0
+              ? ""
+              : `<span class="absolute text-xs right-2 bottom-2 bg-black p-1 rounded text-white">${getTimeString(
+                  video.others.posted_date
+                )}</span>`
+          }
+          
       </figure>
       <div class="px-0 py-2 flex gap-2">
         <div>
-        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" />
+        <img class="w-10 h-10 rounded-full object-cover" src="${
+          video.authors[0].profile_picture
+        }" />
         </div>
         <div>
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
           <P class="text-gray-400">${video.authors[0].profile_name}</P>
-          ${video.authors[0].verified === true ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />` : ""}
+          ${
+            video.authors[0].verified === true
+              ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
+              : ""
+          }
         </div>
         <P></P>
         </div>
@@ -84,12 +119,20 @@ const displayCategories = (categories) => {
   categories.forEach((item) => {
     console.log(item);
     //create a button
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
+    const buttonContainer = document.createElement("div");
 
+    buttonContainer.innerHTML = `
+    <button onclick ="loadCategoryVideos(${item.category_id})" class="btn">
+    ${item.category}
+    </button>
+    `;
+    // button.classList = "btn";
+    // button.innerText = item.category;
+    // button.onclick = () =>{
+    //   alert('Hello')
+    // }
     //  add button to categoryContainer
-    categoryContainer.append(button);
+    categoryContainer.append(buttonContainer);
   });
 };
 
--- test.js
+++ test.js
@@ -1,11 +1,24 @@
-const isVerified = "";
+// const isVerified = "";
 
-// if(isVerified === true){
-//    console.log("User is verified");
-// }
-// else{
-//    console.log("User is not verified");
+// // if(isVerified === true){
+// //    console.log("User is verified");
+// // }
+// // else{
+// //    console.log("User is not verified");
 
-// }
+// // }
 
-console.log(`${isVerified === true? "User is verified" : "User is not verified"}`);
+// console.log(`${isVerified === true? "User is verified" : "User is not verified"}`);
+
+
+//  second to hours and minutes
+function getTimeString(time){
+   // get hour and rest seconds
+   const hour = parseInt(time/ 3600);
+   let remainingSecond = time % 3600;
+   const minute = parseInt(remainingSecond / 60);
+   remainingSecond = remainingSecond % 60;
+   return `${hour} hour ${minute} minute ${remainingSecond} second ago`
+}
+
+console.log(getTimeString(7865));
